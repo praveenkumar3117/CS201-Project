74294667cd9d8cc2860625c676dc87f497076897
praveenkumar3117 <sainipraveen818@gmail.com>
2021-11-14 11:00:57 +0530

    Added frequency counter

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+unordered_map<char,long long int>freq;
+
+void calculate_freq(vector<char>input)
+{
+    long long int i;
+    long long int n=input.size();
+    for(i=0;i<n;i++)
+    {
+        freq[input[i]]++;
+    }
+
+}
 
 int main()
 {
-string filename("file.txt");
+    string filename("file.txt");
     vector<char> input;
     char byte = 0;
 
@@ -17,12 +29,20 @@ string filename("file.txt");
     while (input_file.get(byte)) {
         input.push_back(byte);
     }
-    for(int i=0;i<input.size();i++)
+    for(long long int i=0;i<input.size();i++)
     {
         cout<<input[i]<<" ";
     }
     cout << endl;
     input_file.close();
+    calculate_freq(input);
+    for(auto & x: freq)
+    {
+        cout<<x.first<<": "<<x.second<<endl;
+    }
+
+
+
 
     return EXIT_SUCCESS;
 
