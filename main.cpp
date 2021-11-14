611ab7d64c0bc00e4a945fecb594c6356fc361fe
praveenkumar3117 <sainipraveen818@gmail.com>
2021-11-14 16:24:36 +0530

    Generated codes for the character

--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-unordered_map<char,long long int>freq;
+unordered_map<char,long long int>f;
 
-void calculate_freq(vector<char>input)
-{
-    long long int i;
-    long long int n=input.size();
-    for(i=0;i<n;i++)
+void show_codes(struct huffmanNode * root,string s);
+void create_codes(unordered_map<char,long long int>f);
+void calculate_freq(vector<char>input);
+
+struct huffmanNode{
+    char val;
+    long long int freq;
+    struct huffmanNode * left;
+    struct huffmanNode* right;
+};
+
+
+struct fun1 {
+ 
+    bool operator()(struct huffmanNode* l,struct  huffmanNode* r)
+ 
     {
-        freq[input[i]]++;
+        return (l->freq>r->freq);
     }
+};
 
-}
+
 
 int main()
 {
     string filename("file.txt");
     vector<char> input;
     char byte = 0;
-
+    long long int i;
     ifstream input_file(filename);
     if (!input_file.is_open()) {
         cerr << "Could not open the file - '"
@@ -36,17 +48,79 @@ int main()
     cout << endl;
     input_file.close();
     calculate_freq(input);
-    for(auto & x: freq)
+    cout<<"Frequency of characters in the file:\n";
+    for(auto x: f)
     {
-        cout<<x.first<<": "<<x.second<<endl;
+        cout<<x.first<<" : "<<x.second<<endl;
+    }
+    
+    create_codes(f);
+
+
+
+    return EXIT_SUCCESS;    
+}
+void show_codes(struct huffmanNode * root,string s)
+{
+    if(!root)
+    {
+        return;
+    }
+    if(root->val!='$')
+    {
+        cout<<root->val<<": "<<s<<"\n";
+    }
+    if(root->val=='$' && root->right==NULL && root->left==NULL)
+    {
+        cout << root->val << ": " << s << "\n";
+    }
+ 
+    show_codes(root->left, s + "0");
+    show_codes(root->right, s + "1");
+}
+void calculate_freq(vector<char>input)
+{
+    long long int i;
+    long long int n=input.size();
+    for(i=0;i<n;i++)
+    {
+        
+        f[input[i]]++;
+        
     }
 
+}
+void create_codes(unordered_map<char,long long int>f)
+{
+    priority_queue<struct huffmanNode*, vector<struct huffmanNode*> ,fun1> heap;
+  
+    long long int i;
+    for(auto x: f)
+    {
+        struct huffmanNode* temp=new huffmanNode();
+        temp->val=x.first;
+        temp->freq=x.second;
+        temp->left=NULL;
+        temp->right=NULL;
+        heap.push(temp);
 
-
-
-    return EXIT_SUCCESS;
-
-
-
-   
+    }
+    while(heap.size()!=1)
+    {
+        struct huffmanNode* l=heap.top();
+        heap.pop();
+        cout<<"Nodes used in combining this time:\n";
+        cout<<l->val<<" : "<<l->freq<<endl;
+        struct huffmanNode* r=heap.top();
+        heap.pop();
+        cout<<r->val<<" : "<<r->freq<<endl<<endl;
+        struct huffmanNode* parent=new huffmanNode();
+        parent->val='$';
+        parent->freq=l->freq + r->freq;
+        parent->left=l;
+        parent->right=r;
+        heap.push(parent);
+    }
+    cout<<"Huffman Codes for characters:\n";
+    show_codes(heap.top(),"");
 }
